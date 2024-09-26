15ccc04d7d28f8c6309a80b7ad10ae00a3d08d57
Ghassaan <ghassaank1@gmail.com>
2024-09-26 16:07:36 +0500

    Another test commit

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 4000
 
 
 app.get('/', (req, res) => {
-    res.send('My First Deploy!')
+    res.send(`<h1>Hello World! Here's my first deploy!</h1>`)
 })
 
 app.listen(process.env.PORT, ()=>{
